1761d30cdba73e8dd1b6083385a631d4614bed87
moniqueBandas <moniquedbandas@gmail.com>
2024-01-16 15:36:09 -0300

    adicionado arquivos `.prettierignore` e configurações do prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Treinamento para um curso!
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-function Home(){
-    return <h1>oi fofinhoinhu :3!!! s2 </h1>
+function Home() {
+  return <h1>oi fofinhoinhu :3!!! s2 </h1>;
+}
+
+function teste() {
+  console.log("indentação erradinha");
 }
 export default Home;
